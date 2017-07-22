c119480d1c70f721e8f60941957cef3c852157ad
jacob <jgeiman12@yahoo.com>
2017-07-22 11:25:12 -0400

    add db model for storing containers

--- factorio_manager/containers/models.py
+++ factorio_manager/containers/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Container(models.Model):
+    container_id = models.CharField(max_length=32, primary_key=True)
+    image = models.CharField(max_length=255)
+    created = models.DateTimeField()
+    started = models.DateTimeField()
+    status = models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
--- factorio_manager/containers/tasks.py
+++ factorio_manager/containers/tasks.py
@@ -1,7 +1,23 @@
 from __future__ import absolute_import
+
+import docker
+
 from celery import shared_task
+from django.conf import settings
+from containers.models import Container
 
 
 @shared_task
-def test(param):
-    return 'The test task executed with argument "%s" ' % param
+def fetch_containers():
+    client = docker.from_env(version=settings.DOCKER_API_VERSION)
+    all_containers = client.containers.list(all=True)
+    for c in all_containers:
+        new_c = Container.objects.create(
+            container_id=c.id,
+            image=c.attrs['Config']['Image'],
+            created=c.attrs['Created'],
+            status=c.attrs['State']['Status'],
+            name=c.attrs['Name'],
+        )
+        new_c.save()
+

821a5bd72fd4b532bcc9aac5e42a826143f46309
jacob <jgeiman12@yahoo.com>
2017-07-25 20:39:05 -0400

    fixes

--- factorio_manager/containers/tasks.py
+++ factorio_manager/containers/tasks.py
@@ -12,11 +12,10 @@ def fetch_containers():
     client = docker.DockerClient(base_url=settings.DOCKER_SOCKET_URL, version=settings.DOCKER_API_VERSION)
     all_containers = client.containers.list(all=True)
     for c in all_containers:
-        new_c = Container.objects.get_or_create(id=c.id)
-        new_c.image=c.attrs['Config']['Image']
-        new_c.created = c.attrs['Created']
-        new_c.started = c.attrs['State']['StartedAt']
-        new_c.status = c.attrs['State']['Status']
-        new_c.name = c.attrs['Name']
-        new_c.save()
-
+        container, created = Container.objects.update_or_create(
+            id=c.id,
+            image=c.attrs['Config']['Image'],
+            created=c.attrs['Created'],
+            started=c.attrs['State']['StartedAt'],
+            status=c.attrs['State']['Status'],
+            name=c.attrs['Name'])
